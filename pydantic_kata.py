33a642cf22e46e6cbe0b165e8d23a957a3a2a8f7
rachellvns <rachellvannessachr@gmail.com>
2026-07-30 01:13:17 +0800

    Added OffPageSEO with validation demo

--- pydantic_kata.py
+++ pydantic_kata.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, ValidationError
 
 # SEO Audit has 3 pillars: 
 # technical, on-page, off-page
-    
+
 # 1st pillar
 class TechnicalSEO(BaseModel):
     page_speed: float = Field(gt=0)
@@ -10,13 +10,23 @@ class TechnicalSEO(BaseModel):
     mobile_friendly: bool
     crawled_pages: int = Field(default=0, ge=0)
 
-good_data = '{"page_speed": 5.3, "broken_links": 2, "mobile_friendly": "True", "crawled_pages": 7}'
-tec = TechnicalSEO.model_validate_json(good_data)
+# 2nd pillar
+class OffPageSEO(BaseModel):
+    backlinks : int = Field(default=0, ge=0)
+    referring_domains: int = Field(default=0, ge=0)
+    brand_mentions: int = Field(default=0, ge=0)
+
+good_data = '{"backlinks": 5, "referring_domains": 2}'
+tec = OffPageSEO.model_validate_json(good_data)
 print(tec)
 
-bad_data = '{"broken_links": 3, "mobile_friendly": "False", "crawled_pages": 8}'
+default_data = '{''}'
+tec = OffPageSEO.model_validate_json(default_data)
+print(tec)
+
 try:
-    tec = TechnicalSEO.model_validate_json(bad_data)
+    bad_data = '{"backlinks": 3, "referring_domains": 2, "brand_mentions": "False"}'
+    tec = OffPageSEO.model_validate_json(bad_data)
     print(tec)
 except ValidationError as e:
-    print("Rejected, missing required field!")
+    print("Rejected, contain incompatible field!")
